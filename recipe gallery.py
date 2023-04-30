2ee8c3545333b08c1e8f6441fe3f11f3cffc06db
l3vth <132221005+l3vth@users.noreply.github.com>
2023-05-01 00:30:20 +0200

    Add files via upload

---
+++ recipe gallery.py
@@ -0,0 +1,182 @@
+print("")
+print("")
+##top
+
+print("(1) Easy Pancakes ") 
+print("")
+print("(2) Easy Tropical cheesecake ")
+print("")
+print("(3) Easy cheesy 2-minute noodles")
+print("")
+
+choice = input("which recipe do you want? ")
+print("")
+print("")
+print("")
+
+
+#recipe1
+
+for recipe1 in ["*EASY PANCAKES*",
+    "",
+    "Ingredients:",
+    "",
+    "2 eggs",
+    "",
+    "1 3/4 cup milk",
+    "",
+    "1 tsp vanilla essence",
+    "",
+    "2 cups self-raising flour",
+    "",
+    "1/3 cup caster sugar",
+    "",
+    "Butter, for frying, plus extra, to serve",
+    "",
+    "Maple syrup, to serve"
+    "",
+    "",
+    "*3 Steps Method*",
+    "",
+    "step 1",
+    "Whisk eggs, milk and vanilla together in a jug.", 
+    "Sift flour into a large bowl. Stir in sugar. Make a well in the centre. Add milk mixture. Whisk until just combined."
+    "",
+    "",
+    "Step 2",
+    "Heat a large non-stick frying pan over medium heat. Grease pan with butter or spray with cooking oil.",
+    "Using 1/4 cup mixture per pancake, cook 2 pancakes for 2 minutes or until bubbles appear on surface.",
+    "Turn and cook for a further 1-2 minutes or until cooked through. Transfer to a plate. Cover loosely with foil to keep warm.",
+    "Repeat with remaining mixture, greasing pan with butter or cooking oil between batches."
+    "",
+    "",
+    "Step 3",
+    "Serve with maple syrup and extra butter to taste :)."
+
+
+
+
+
+]: 
+    if choice == str(1) :
+     print(recipe1)
+
+
+#recipe2
+
+for recipe2 in ["EASY TROPICAL CHEESECAKE",
+    "",
+    "Ingredients",
+    "",
+    "60g butter, melted"
+
+    "2 tbsp cold water",
+    "",
+    "2 1/2 tsp gelatine",
+    "",
+    "2 x 250g pkts cream cheese, chopped at room temperature",
+    "",
+    "3/4 cup caster sugar",
+    "",
+    "1 tbsp lime rind",
+    "",
+    "4 tbsp lime juice",
+    "",
+    "300 ml thickened cream",
+    "",
+    "1 large mango",
+    "",
+    "14 butternut snap cookies, bro ken (2/3 of a 250g pkt)",
+    "",
+    "1/2 pineapple",
+    "",
+    "2 passionfruit pulp, reserved",
+    "",
+    "*5 Steps Method*",
+    "",
+    "Step 1",
+    "Lightly grease a 22cm springform tin and line base with non-stick baking paper.",
+    "",
+    "Step 2",
+    "Place biscuits into a food processor and process until finely chopped. Add butter and process until combined.," 
+    "Use back of a spoon to press the biscuit mixture over the base of the prepared tin. Refrigerate the base while you make the filling."
+    "",
+    "Step 3",
+    "Pour cold water into a small bowl. Sprinkle the gelatine over.",
+    "When gelatine has softened, stand the bowl in a pan of hot water and whisk with a fork until dissolved.",
+    "",
+    "Step 4",
+    "Using electric beaters, beat the cream cheese, sugar, lime juice and rind until smooth.", 
+    "Add cream and the gelatine mixture and beat briefly to combine.",
+    "Pour over the biscuit base. Cover and refrigerate for about 4 hrs until firm.",
+    "",
+    "Step 5",
+    "Peel and core the pineapple and cut lengthways into thin slices. Peel and cut the mango flesh into thin slices.", 
+    "Remove cheesecake from springform pan and place on serving platter. Arrange fruit on top, drizzle with passionfruit pulp.",
+    "",
+    "Enjoy!"
+]:
+   if choice == str(2):
+    print(recipe2)
+
+
+#recipe3
+for recipe3 in ["EASY CHEESY 2 MINUTE NOODLES",
+    "",
+    "Ingredients:",
+    "",
+    "1 tbsp extra virgin olive oil",
+    "",
+    "20g butter",
+    "",
+    "1 brown onion, finely chopped",
+    "",
+    "4 middle bacon rashers, trimmed, roughly chopped",
+    "",
+    "1 tbsp plain flour",
+    "",
+    "4 x 72g packets Maggi 2 Minute Noodles Chicken Flavour",
+    "",
+    "2 cups milk",
+    "",
+    "1 cup frozen peas",
+    "",
+    "1 cup grated Devondale Colby Cheese Block (500g)",
+    "",
+    "1/4 cup fresh flat-leaf parsley leaves, chopped",
+    "",
+    "1 tsp lemon zest",
+    "",
+    "*4 Steps Method*",
+    "",
+    "Step 1",
+    "Heat oil and butter in a large, deep frying pan over medium-high heat. Add onion and bacon.",
+    "Cook, stirring occasionally, for 5 minutes or until onion softens. ",
+    "Transfer 2 tablespoons onion mixture to a small bowl. Cover to keep warm.",
+    "",
+    "Step 2",
+    "Add flour and 2 of the chicken flavour sachets from noodles to pan (discard remaining flavour sachets).",
+    "Cook, stirring, for 1 minute. ",
+    "Reduce heat to medium. Gradually stir in milk and 2 cups water. Bring to a simmer. ",
+    "Add noodles. Cook, stirring occasionally to break up noodles, for 2 minutes or until noodles are tender.",
+    "",
+    "Step 3",
+    "Add peas and cheese. Cook for a further 1 to 2 minutes or until peas are bright green and tender. ",
+    "Add parsley and lemon zest to reserved onion mixture. Season with salt and pepper. Toss to combine.",
+    "",
+    "Step 4",
+    "Remove noodles from heat. Sprinkle with onion mixture.", 
+    "Serve and enjoy."
+                
+   
+]:
+   if choice == str(3):
+    print(recipe3)
+
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+input("press any button to quit")
